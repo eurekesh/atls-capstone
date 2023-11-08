f4fcd9970451a2c28413b0045435724219dee337
eurekesh <asdu8905@colorado.edu>
2023-11-08 12:04:53 -0700

    runs

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ AF_DCMotor motor1(1);
 AF_DCMotor motor2(2);
 AF_DCMotor motor3(3);
 AF_DCMotor motor4(4);
+AF_DCMotor all_wheels[] = {motor1, motor2, motor3, motor4};
 
 // int Xpot;
 // int Ypot;
@@ -17,7 +18,12 @@ AF_DCMotor motor4(4);
 // sets the max speed and the joystick values
 int speed = 255;
 
-int grid[5][5] = {{0}};
+int grid[6][6] = {{0}};
+int curr_position[2] = {3,3};
+
+void allWheels(int);
+void leftTurn(int);
+void rightTurn(int);
 
 void setup() {
   // put your setup code here, to run once:
@@ -34,17 +40,45 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // radio.startListening();
 
-    // forwar
-        motor1.setSpeed(speed);
-        motor1.run(FORWARD);
-        motor2.setSpeed(speed);
-        motor2.run(FORWARD);
-        motor3.setSpeed(speed);
-        motor3.run(FORWARD);
-        motor4.setSpeed(speed);
-        motor4.run(FORWARD);
-        Serial.println("forward");
-   
+  // allWheels(100);
+  
+}
+
+
+void allWheels(int speed)
+{
+  for(int i = 0; i < 4; i++)
+  {
+    all_wheels[i].setSpeed(speed);
+    all_wheels[i].run(FORWARD);
+  }
+}
+
+void leftTurn(int speed)
+{
+  for(int i = 0; i < 4; i++)
+  {
+    int using_speed = speed;
+    if (i < 2)
+    {
+      using_speed /= 3;
+    }
+    all_wheels[i].setSpeed(using_speed);
+    all_wheels[i].run(FORWARD);
+  }
+}
+
+void rightTurn(int speed)
+{
+  for(int i = 0; i < 4; i++)
+  {
+    int using_speed = speed;
+    if (i > 2)
+    {
+      using_speed /= 3;
+    }
+    all_wheels[i].setSpeed(using_speed);
+    all_wheels[i].run(FORWARD);
+  }
 }
